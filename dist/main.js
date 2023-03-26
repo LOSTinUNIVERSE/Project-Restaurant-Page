e7fab53e48eaa45407005eeeb6e78bd4feed6208
abdulla <abdullaabdullaev0803@gmail.com>
2023-03-26 17:59:52 +0300

    add basic layout

---
+++ dist/main.js
@@ -0,0 +1,1 @@
+(()=>{const e=document.getElementById("content"),n=document.createElement("header"),t=document.createElement("main"),d=document.createElement("footer"),c=document.createElement("div");c.id="innerBlock",e.appendChild(n),e.appendChild(t),e.appendChild(d),t.appendChild(c)})();
---
+++ src/index.js
@@ -0,0 +1,16 @@
+// import './style.css';
+const home = () => {
+    const content = document.getElementById("content")
+    const header = document.createElement('header')
+    const main = document.createElement('main')
+    const footer = document.createElement('footer')
+    const innerBlock = document.createElement('div')
+    const appending = (() => {
+        innerBlock.id = 'innerBlock'
+        content.appendChild(header)
+        content.appendChild(main)
+        content.appendChild(footer)
+        main.appendChild(innerBlock)
+    })()
+}
+home()
